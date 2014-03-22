954f49fc0a854777c3bee9dd680f119387c11304
Graham Holtslander <gholtslander@vendasta.com>
2014-03-22 09:22:27 -0600

    Adding files possibly

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dev_appserver.py src --datastore_path="~/.datastores/repeating_expiries" --port=8080
+# See here for more info https://developers.google.com/appengine/docs/python/tools/devserver
