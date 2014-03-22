ca0984fb7b2f42a88bdbc693c907f2a6cf6ee996
Graham Holtslander <menello@gmail.com>
2014-03-22 11:14:32 -0600

    Adding a file

---
+++ src/app/views/user.py
@@ -0,0 +1,38 @@
+"""
+User views
+"""
+from app.views import TemplatedView
+from app.domain.user import check_and_return_user, get_current_user
+from app.workflow.user import get_or_create_user
+
+class NewUserView(TemplatedView):
+    """ New user view, gets called when someone signs in """
+
+    def get(self):
+        """ GET """
+        user, ndb_user, in_datastore = check_and_return_user()
+
+        if in_datastore:
+            # Not making someone new, redirect back home
+            self.redirect('/')
+
+        context = {
+            "new_user": not in_datastore,
+            "user": user,
+            "ndb_user": ndb_user
+        }
+
+        self.render_response("user.html", **context)
+
+    def post(self):
+        """ POST """
+        name = self.request.POST.get('name')
+        email = self.request.POST.get('email')
+        users_user = get_current_user()
+
+        user = get_or_create_user(name, email, users_user.user_id())
+
+        import logging
+        logging.info("I made something? {0}".format(user))
+
+        self.redirect('/')
